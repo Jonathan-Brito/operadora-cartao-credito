3cae116196557c05ab3c348bcabf72972060d0bb
Jonathan Brito Costa <jonathan.costa@itsgroup.com.br>
2025-07-20 16:23:08 -0300

    Projeto criado

---
+++ src/main/java/br/com/brito/operadoracartaocredito/OperadoraCartaoCreditoApplication.java
@@ -0,0 +1,13 @@
+package br.com.brito.operadoracartaocredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperadoraCartaoCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperadoraCartaoCreditoApplication.class, args);
+	}
+
+}
